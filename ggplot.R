14584a9d2ff8beea8ac7b7b568fb781cee243de4
Victoria de los Angeles Peñafiel Quito <vpenafielq@est.ups.edu.ec>
2023-04-02 20:10:18 -0500

    Control de las propiedades de los ejes

--- ggplot.R
+++ ggplot.R
@@ -8,3 +8,17 @@ x y
 ggplot()+geom_point(data=dat,aes(x=x,y=y),size=5,color="blue")
 
 ggplot()+geom_point(data=dat,aes(x=x,y=y),size=10,color="red")
+
+#Control de Propiedades
+x<-c(2,5,1)
+y<-c(6,4,9)
+
+dat<-data.frame(x,y)
+dat
+  x y
+1 2 6
+2 5 4
+3 1 9
+
+ggplot()+geom_point(data=dat,aes(x=x,y=y),size=5,color="blue")
+ggplot()+geom_point(data=dat,aes(x=x,y=y),size=10,color="red")
